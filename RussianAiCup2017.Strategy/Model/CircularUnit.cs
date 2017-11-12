3506df99c89d25a77ab829adc929a8cd11fb3040
Maxim Golubev <maxim.gol23@gmail.com>
2017-11-12 16:17:39 +0100

    Language pack and local-runner update

--- RussianAiCup2017.Strategy/Model/CircularUnit.cs
+++ RussianAiCup2017.Strategy/Model/CircularUnit.cs
@@ -1,15 +1,14 @@
-namespace Com.CodeGame.CodeWars2017.DevKit.CSharpCgdk.Model
-{
-	public abstract class CircularUnit : Unit
-	{
-		private readonly double radius;
+using System;
 
-		protected CircularUnit(long id, double x, double y, double radius)
-			: base(id, x, y)
-		{
-			this.radius = radius;
-		}
+namespace Com.CodeGame.CodeWars2017.DevKit.CSharpCgdk.Model {
+    public abstract class CircularUnit : Unit {
+        private readonly double radius;
 
-		public double Radius => radius;
-	}
+        protected CircularUnit(long id, double x, double y, double radius)
+                : base(id, x, y) {
+            this.radius = radius;
+        }
+
+        public double Radius => radius;
+    }
 }
--- RussianAiCup2017.Strategy/Model/PlayerContext.cs
+++ RussianAiCup2017.Strategy/Model/PlayerContext.cs
@@ -1,17 +1,16 @@
-namespace Com.CodeGame.CodeWars2017.DevKit.CSharpCgdk.Model
-{
-	public sealed class PlayerContext
-	{
-		private readonly Player player;
-		private readonly World world;
+using System;
 
-		public PlayerContext(Player player, World world)
-		{
-			this.player = player;
-			this.world = world;
-		}
+namespace Com.CodeGame.CodeWars2017.DevKit.CSharpCgdk.Model {
+    public sealed class PlayerContext {
+        private readonly Player player;
+        private readonly World world;
 
-		public Player Player => player;
-		public World World => world;
-	}
+        public PlayerContext(Player player, World world) {
+            this.player = player;
+            this.world = world;
+        }
+
+        public Player Player => player;
+        public World World => world;
+    }
 }
