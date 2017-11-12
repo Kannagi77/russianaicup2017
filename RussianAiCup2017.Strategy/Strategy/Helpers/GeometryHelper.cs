420bc138eb7f0c50ec2cc757b5260a1aa29bdb97
Maxim Golubev <maxim.gol23@gmail.com>
2017-11-13 00:48:02 +0100

    Bug fixed
    v6

--- RussianAiCup2017.Strategy/Strategy/Helpers/GeometryHelper.cs
+++ RussianAiCup2017.Strategy/Strategy/Helpers/GeometryHelper.cs
@@ -11,6 +11,11 @@ namespace Com.CodeGame.CodeWars2017.DevKit.CSharpCgdk.Strategy.Helpers
 			return u.GetDistanceTo(point.X, point.Y);
 		}
 
+		public static Point2D ToPoint(this Unit unit)
+		{
+			return new Point2D(unit.X, unit.Y);
+		}
+
 		public static Point2D GetCenterPoint(this IEnumerable<Unit> units)
 		{
 			var list = units.ToList();
@@ -26,12 +31,25 @@ namespace Com.CodeGame.CodeWars2017.DevKit.CSharpCgdk.Strategy.Helpers
 
 		public static Unit GetClosest(this IEnumerable<Unit> units, Point2D point)
 		{
-			return units.ToDictionary(u => u.GetDistanceTo(point), u => u).OrderBy(p => p.Key).First().Value;
+			return GetUnitsByDistance(units, point).OrderBy(p => p.Key).First().Value;
 		}
 
-		public static Unit GetClosestAtMinimumRange(this IEnumerable<Unit> units, Unit point, double range)
+		public static Unit GetClosestAtMinimumRange(this IEnumerable<Unit> units, Unit unit, double range)
 		{
-			return units.ToDictionary(u => u.GetDistanceTo(point), u => u).OrderBy(p => p.Key).FirstOrDefault(p => p.Key > range).Value;
+			return GetUnitsByDistance(units, unit.ToPoint()).OrderBy(p => p.Key).FirstOrDefault(p => p.Key > range).Value;
+		}
+
+		private static Dictionary<double, Unit> GetUnitsByDistance(IEnumerable<Unit> units, Point2D point)
+		{
+			var unitsByDistance = new Dictionary<double, Unit>();
+			foreach (var unit in units.ToList())
+			{
+				var distance = unit.GetDistanceTo(point);
+				if (unitsByDistance.ContainsKey(distance))
+					continue;
+				unitsByDistance.Add(distance, unit);
+			}
+			return unitsByDistance;
 		}
 	}
 }
